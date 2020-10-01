aed529df53c10f98a731829ee15625650089c125
Tanish <juthugasai123@gmail.com>
2020-10-01 20:50:35 +0530

    hower

---
+++ scarch.js
@@ -0,0 +1,66 @@
+//<a style="background-color:rgb(0, 137, 216)" href="index.html">Projects </a>
+//<a style="background-color:rgb(0, 137, 216)"href="aboutme.html">About Me</a>
+
+/*
+NUMBER SYSTEM
+
+Decimal Number System = 10
+10 digits = 0-9
+
+56 = 50 + 6
+   = 5*10^1 + 6*10^0
+
+756 = 700 + 50 + 6
+    = 7*10^2+ 5*10^1 + 6*10^0
+
+10^0 = 1
+10^1 = 10
+10^2 = 100
+10^3 = 1000 
+
+Binary Number System = 2
+2 digits = 0,1
+
+101 = 1*2^2 + 0*2^1 + 1*2^0
+
+
+Hexadecimal Number System(16)
+Hexa = 6 Decimal = 10
+0-9
+10: A
+11: B
+12: C
+13: D
+14: E
+15: F
+0-F  = 16 digits
+
+AD = A*16^1 + D*16^0
+    = 10*16 + 13*1
+    = 160+13
+    =173
+
+FE = 15*16^1 + 14*16^0
+   = 240 + 14
+   = 254
+
+255 in Hexadecimal?
+Quotient = 15 = F
+Remainder = 15 = F
+255 = FF
+
+00
+01
+
+# - represents hexadecimal number
+black = rgb (0, 0,  0)
+     = #000000
+White = #FFFFFF
+
+
+a:link
+    To customize the style of the links
+a:visited
+    To customize the the style of the visited links
+
+*/
